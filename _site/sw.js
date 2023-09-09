c2089a15126ae682162e9b99891b0f1e79304f21
iniakunhuda <iniakunhuda@gmail.com>
2023-09-10 01:39:48 +0700

    init website

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230910.013328';
+const cacheName = 'chirpy-20230910.013941';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
